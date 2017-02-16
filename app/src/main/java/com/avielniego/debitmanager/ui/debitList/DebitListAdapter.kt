dcc4c61e7415a8d3b7da427e745ebae6130a20d3
Aviel <avielniego@gmail.com>
2017-02-17 00:32:47 +0200

    feature: Recycler view with adapter added to DebitListFragment

---
+++ app/src/main/java/com/avielniego/debitmanager/ui/debitList/DebitListAdapter.kt
@@ -0,0 +1,36 @@
+package com.avielniego.debitmanager.ui.debitList
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.avielniego.debitmanager.R
+import com.avielniego.debitmanager.messageParser.Debit
+import java.text.NumberFormat
+
+class DebitListAdapter(debits: List<Debit> = emptyList()): RecyclerView.Adapter<DebitListAdapter.ViewHolder>() {
+
+    var debits = debits
+        set(debits) {
+            this.debits = debits
+            notifyDataSetChanged()
+        }
+
+    override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
+        holder!!.debitSumTextView.text = NumberFormat.getInstance().format(debits[position].sum)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent!!.context).inflate(R.layout.debit_list_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return debits.count()
+    }
+
+    class ViewHolder(val view: View): RecyclerView.ViewHolder(view) {
+        val debitSumTextView = view.findViewById(R.id.debit_sum) as TextView
+    }
+}
--- app/src/main/java/com/avielniego/debitmanager/ui/debitList/DebitListFragment.kt
+++ app/src/main/java/com/avielniego/debitmanager/ui/debitList/DebitListFragment.kt
@@ -2,12 +2,17 @@ package com.avielniego.debitmanager.ui.debitList
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.avielniego.debitmanager.R
 
 class DebitListFragment: Fragment() {
+
+    val debitListAdapter = DebitListAdapter()
+
     companion object{
         fun new(): DebitListFragment {
             val fragment = DebitListFragment()
@@ -28,6 +33,14 @@ class DebitListFragment: Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater!!.inflate(R.layout.debit_list_fragment, container, false)
+        val v = inflater!!.inflate(R.layout.debit_list_fragment, container, false)
+        initRecyclerView(v)
+        return v
+    }
+
+    private fun initRecyclerView(v: View) {
+        val recyclerView = v.findViewById(R.id.debit_recycler_view) as RecyclerView
+        recyclerView.layoutManager = LinearLayoutManager(recyclerView.context)
+        recyclerView.adapter = debitListAdapter
     }
 }
