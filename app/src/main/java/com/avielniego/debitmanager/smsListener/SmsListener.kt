660e6857cf57627c546847e13e4daf47cd67e4cc
Aviel <avielniego@gmail.com>
2017-02-16 14:33:40 +0200

    make SmsListener use MessageConsumer

--- app/src/main/java/com/avielniego/debitmanager/smsListener/SmsListener.kt
+++ app/src/main/java/com/avielniego/debitmanager/smsListener/SmsListener.kt
@@ -5,21 +5,20 @@ import android.content.Context
 import android.content.Intent
 import android.provider.Telephony
 import android.telephony.SmsMessage
-import android.widget.Toast
 import com.avielniego.debitmanager.messageParser.Message
-import com.avielniego.debitmanager.messageParser.credirCardParser.MasterCardParser
+import com.avielniego.debitmanager.messageParser.MessageConsumer
+import com.avielniego.debitmanager.messageParser.debitStorage.MemoryDebitStorage
 
 
 class SmsListener : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         if (intent.action != Telephony.Sms.Intents.SMS_RECEIVED_ACTION)
             return
-        parseMessages(context, Telephony.Sms.Intents.getMessagesFromIntent(intent))
+        parseMessages(Telephony.Sms.Intents.getMessagesFromIntent(intent))
     }
 
-    private fun parseMessages(context: Context, smsMessages: Array<SmsMessage>) {
-        smsMessages
-                .map { MasterCardParser().parse(Message(it.originatingAddress, it.messageBody)) }
-                .forEach { Toast.makeText(context, it.toString(), Toast.LENGTH_LONG).show() }
+    private fun parseMessages(smsMessages: Array<SmsMessage>) {
+        val consumer = MessageConsumer(MemoryDebitStorage())
+        smsMessages.forEach { consumer.consume(Message(it.originatingAddress, it.messageBody)) }
     }
 }
